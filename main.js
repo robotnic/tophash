9a2b119e0c9e1bf1ff5b4d1e21b43951c48ed58b
Bernhard Zwischenbrugger <bz@datenkueche.com>
2016-02-14 22:51:30 +0100

    formatted

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var app = angular.module('tophash', ['ui.router']);
 app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/");
   $stateProvider.state({ name: 'home', url: '/', controller: function() { }, template: '<h3>Features</h3><li>backbutton</li><li> forwardbutton</li><li> deeplinking</li><li>bookmarking</li><li>history</h1><li>send link (to helpdesk) </li><h3>Missing</h3>hash namespaces, well known deeplinks</div>'});
-  $stateProvider.state({ name: 'page', url: '/page', controller: function() { }, template: '<h3>Template 1</h3>Level 1<div  ui-view></div>level 1'});
+  $stateProvider.state({ name: 'page', url: '/page', controller: function() { }, template: '<div style="border:1px solid lime;padding:8px"><h3>Template 1</h3>Have you seen the hash in the url field of your browser?<div  ui-view></div><a ui-sref="page.foo">/page/foo</a> <a ui-sref="page">/page</a></div>'});
   $stateProvider.state({ name: 'page.foo', url: '/foo', controller: function() { }, template: '<div style="border:1px solid green;padding:8px"><h3>Template 2</h3><b>Nested state</b> <a href="#/page/foo/12">12</a> <a ui-sref="page.foo.id({id:27})">27</a><div  ui-view></div></div>'});
   $stateProvider.state({ name: 'page.foo.id', url: '/:id', controller: function() { }, template: '<div style="border:1px solid blue"><h3>Template 3</h3><div style="font-size:20px">$state.params.id: {{$state.params.id}}</div>'});
 });
