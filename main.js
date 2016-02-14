e66ff419be5ceb1f989821ce16c6ef4fea3360f6
Bernhard Zwischenbrugger <bz@datenkueche.com>
2016-02-14 21:49:41 +0100

    formatting

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/");
   $stateProvider.state({ name: 'home', url: '/', controller: function() { }, template: '<h3>Features</h3><li>backbutton</li><li> forwardbutton</li><li> deeplinking</li><li>bookmarking</li><li>history</h1><li>send link (to helpdesk) </li><h3>Missing</h3>hash namespaces, well known deeplinks</div>'});
   $stateProvider.state({ name: 'page', url: '/page', controller: function() { }, template: '<h3>Template 1</h3>Level 1<div  ui-view></div>level 1'});
-  $stateProvider.state({ name: 'page.foo', url: '/foo', controller: function() { }, template: '<div style="border:1px solid green"><h3>Template 2</h3><b>Nested state</b> <a href="#/page/foo/12">12</a> <a ui-sref="page.foo.id({id:27})">27</a><div  ui-view></div></div>'});
-  $stateProvider.state({ name: 'page.foo.id', url: '/:id', controller: function() { }, template: '<div style="border:1px solid blue"><h3>Template 3</h3><div style="font-size:50px">$state.params.id: {{$state.params.id}}</div>'});
+  $stateProvider.state({ name: 'page.foo', url: '/foo', controller: function() { }, template: '<div style="border:1px solid green;padding:4px"><h3>Template 2</h3><b>Nested state</b> <a href="#/page/foo/12">12</a> <a ui-sref="page.foo.id({id:27})">27</a><div  ui-view></div></div>'});
+  $stateProvider.state({ name: 'page.foo.id', url: '/:id', controller: function() { }, template: '<div style="border:1px solid blue"><h3>Template 3</h3><div style="font-size:20px">$state.params.id: {{$state.params.id}}</div>'});
 });
 
 // Adds state change hooks; 
